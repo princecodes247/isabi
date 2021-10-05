f03a64c2fd3a395f9f76db5023241da969ab9be3
princecodes247 <princecodes247@gmail.com>
2021-10-05 05:00:35 -0400

    fix: navbar bugs

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 let ham = document.querySelector(".navbar__ham");
 let nav = document.querySelector(".navbar nav");
+let navbar = document.querySelector(".navbar");
 
 
 ham.addEventListener("click", ()=> {
     nav.classList.toggle("open");
     ham.classList.toggle("open");
 })
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navbar.classList.add("navbar--small");
+  } else {
+    navbar.classList.remove("navbar--small");
+  }
+} 
